b6aa4c1064ab4537196d2c1c86222a87696b50ab
Alexei Znamensky <russoz@cpan.org>
2011-06-24 17:59:49 +0000

    Import of RUSSOZ/Task-DataFlow-0.001.tar.gz from CPAN.
    gitpan-cpan-distribution: Task-DataFlow
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         RUSSOZ/Task-DataFlow-0.001.tar.gz
    gitpan-cpan-author:       RUSSOZ
    gitpan-cpan-maturity:     released

---
+++ lib/Task/DataFlow.pm
@@ -0,0 +1,58 @@
+package Task::DataFlow;
+
+use strict;
+use warnings;
+
+# ABSTRACT: The DataFlow Module Collection (new flavor)
+
+our $VERSION = '0.001'; # VERSION
+
+
+1;
+
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::DataFlow - The DataFlow Module Collection (new flavor)
+
+=head1 VERSION
+
+version 0.001
+
+=head1 TASK CONTENTS
+
+=head2 DataFlow Core
+
+=head3 L<DataFlow> 1.111750
+
+Version 1.111750 required because: First version with split modules
+
+=head2 DataFlow Converters
+
+=head3 L<DataFlow::Proc::MessagePack> 1.111750
+
+=head3 L<DataFlow::Proc::JSON> 1.111750
+
+=head3 L<DataFlow::Proc::YAML> 1.111750
+
+=head2 DataFlow for Web
+
+=head3 L<DataFlow::Proc::URLRetriever> 1.111750
+
+=head1 AUTHOR
+
+Alexei Znamensky <russoz@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Alexei Znamensky.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
